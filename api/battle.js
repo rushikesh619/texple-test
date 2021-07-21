6bb340012c831af7fae89b6b7f83d268ade46b22
rushikesh619 <rishikeshshinde619@gmail.com>
2021-07-21 20:13:26 +0530

    simple modifications

--- api/battle.js
+++ api/battle.js
@@ -26,54 +26,6 @@ router.get('/getInitialData', async (req, res) => {
     }
 })
 
-router.post('/insertbattle', async (req, res) => {
-    try {
-        if (req.body) {
-            if (req.body.name && req.body.year && req.body.attacker_king && req.body.defender_king) {
-                let battle = new Battle({
-                    name: req.body.name,
-                    year: req.body.year,
-                    battle_number: req.body.battle_number,
-                    attacker_king: req.body.attacker_king,
-                    defender_king: req.body.defender_king,
-                    attacker_1: req.body.attacker_1,
-                    attacker_2: req.body.attacker_2,
-                    attacker_3: req.body.attacker_3,
-                    attacker_4: req.body.attacker_4,
-                    defender_1: req.body.defender_1,
-                    defender_2: req.body.defender_2,
-                    defender_3: req.body.defender_3,
-                    defender_4: req.body.defender_4,
-                    attacker_outcome: req.body.attacker_outcome,
-                    battle_type: req.body.battle_type,
-                    major_death: req.body.major_death,
-                    major_capture: req.body.major_capture,
-                    attacker_size: req.body.attacker_size,
-                    defender_size: req.body.defender_size,
-                    attacker_commander: req.body.attacker_commander,
-                    defender_commander: req.body.defender_commander,
-                    summer: req.body.summer,
-                    location: req.body.location,
-                    region: req.body.region,
-                    note: req.body.note
-                })
-                battle = await battle.save();
-                console.log(battle);
-            } else {
-                throw {
-                    message: "Not allowd!"
-                }
-            }
-        } else {
-            throw {
-                message: "can't get request body"
-            }
-        }
-    } catch (ex) {
-        console.log(ex);
-    }
-})
-
 router.get('/list', async (req, res) => {
     try {
         const result = await battleServices.list();
